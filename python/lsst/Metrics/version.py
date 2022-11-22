4f290d24e9a6c4662238248153927360deb73464
kuropat <kuropat@fnal.gov>
2022-11-21 18:30:43 -0600

    Added more files

--- python/lsst/Metrics/version.py
+++ python/lsst/Metrics/version.py
@@ -4,7 +4,7 @@ from typing import Dict, Optional
 
 __version__: str = "unknown"
 __repo_version__: str = "unknown"
-__fingerprint__: str = "5041918d924d3e997bd83f2a181244b3fe6bdd3c *"
+__fingerprint__: str = "0c4b2a94af11490b8683170fb2c2f7fb48b027b0 *"
 __dependency_versions__: Dict[str, Optional[str]] = {}
 __all__ = (
     "__version__",
